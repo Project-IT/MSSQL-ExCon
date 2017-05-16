6cdd7d0b126521a7164bdc8def890903ea6a999a
jeppeb91 <jeppeb91@hotmail.com>
2017-05-16 19:43:32 +0200

    BugFixing changed typo and BLOB datatype to CLOB

--- src/main/java/com/atlassian/plugins/excon/refapp/EventUpdater.java
+++ src/main/java/com/atlassian/plugins/excon/refapp/EventUpdater.java
@@ -31,7 +31,7 @@ public class EventUpdater {
         ps.setString(12, dbEventObject.getStart());
         ps.setString(13, dbEventObject.getSub_calendar_id());
         ps.setString(14, dbEventObject.getSummary());
-        ps.setNull(15, Types.BLOB);
+        ps.setNull(15, Types.CLOB);
         ps.setString(16, dbEventObject.getUtc_end());
         ps.setString(17, dbEventObject.getUtc_start());
         ps.setString(18, dbEventObject.getVevent_uid());
